ce52241bbed24debc86d20e129d65f449b31d114
LiangChen <shutiao@fanhaoyue.com>
2019-01-18 17:36:04 +0800

    跑任务和发邮件整合

--- requests_test/autoSendMail.py
+++ requests_test/autoSendMail.py
@@ -75,3 +75,31 @@ def send_mail(newfile):
         print("Error: 无法发送邮件")
 
 
+import unittest
+from BSTestRunner import BSTestRunner
+import time
+import ssl
+
+#ssl._create_default_https_context = ssl._create_unverified_context
+
+#指定测试用例和测试报告的路径
+
+test_dir = './test_case'
+report_dir = './reports'
+
+#加载测试用例
+discover = unittest.defaultTestLoader.discover(test_dir, pattern='test*.py')
+
+#定义报告的文件格式
+now = time.strftime("%Y-%m-%d %H_%M_%S_")
+report_name = report_dir + '/' + now + 'test_report.html'
+
+#运行用例并且生成测试报告
+with open(report_name, 'wb') as f:
+    runner = BSTestRunner(stream=f, title=u'测试报告', description=u"用例执行情况")
+
+    runner.run(discover)
+
+newfile = new_file('reports')
+send_mail(newfile)
+
